327aeac4c919ab5d98515d82574ed2b0a935d436
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-02-17 13:00:08 +0300

    Revert "add metadata to document"
    This reverts commit 20d120a89882196fd2d09e9a1bcaf6ef11077fab.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,9 @@
-import { Html, Main, NextScript } from 'next/document'
-import Head from 'next/head'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Berra Karaman</title>
-        <meta name="description" content="A self-taught developer's blog" />
-        <meta name="keywords" content="frontend, developer, outreachy, internship" />
-        <link rel="icon" href="../../public/images/profile.jpg" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
