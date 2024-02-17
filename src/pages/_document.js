96bbbe7189f4a715b691030463ea1b70a4b4722f
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-02-17 13:20:16 +0300

    update metadata

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Berra Karaman's Blog</title>
+        <meta name="author" content="Berra Karaman" />
+        <meta name="description" content="Join me on my journey through frontend development as I share tips, insights, and discoveries." />
+        <meta name="keywords" content="Javascript, React, frontend, developer, outreachy, internship"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
