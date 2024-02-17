b93e01e9f33c748f57c3e651d969bdb05c1ebfa7
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-02-17 13:43:19 +0300

    add google site verification

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <meta name="author" content="Berra Karaman" />
         <meta name="description" content="Join me on my journey through frontend development as I share tips, insights, and discoveries." />
         <meta name="keywords" content="Javascript, React, frontend, developer, outreachy, internship"/>
+        <meta name="google-site-verification" content="jLk66QHI5hgaNW_ak6aXN3HV5NoO1JcC0OoAcRwuM8k" />
       </Head>
       <body>
         <Main />
