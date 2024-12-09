fab0725676ba496a6986404f092c65f302d47b2b
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-12-09 17:38:02 +0300

    add post

--- .contentlayer/generated/Doc/_index.mjs
+++ .contentlayer/generated/Doc/_index.mjs
@@ -3,6 +3,7 @@
 import blog__djangoTemplateInheritanceMdx from './blog__django-template-inheritance.mdx.json' assert { type: 'json' }
 import blog__emailToolsMdx from './blog__emailTools.mdx.json' assert { type: 'json' }
 import blog__introductionMdx from './blog__introduction.mdx.json' assert { type: 'json' }
+import blog__javascriptVarMdx from './blog__javascript-var.mdx.json' assert { type: 'json' }
 import blog__outreachyFirstHalfMdx from './blog__outreachy-first-half.mdx.json' assert { type: 'json' }
 
-export const allDocs = [blog__djangoTemplateInheritanceMdx, blog__emailToolsMdx, blog__introductionMdx, blog__outreachyFirstHalfMdx]
+export const allDocs = [blog__djangoTemplateInheritanceMdx, blog__emailToolsMdx, blog__introductionMdx, blog__javascriptVarMdx, blog__outreachyFirstHalfMdx]
